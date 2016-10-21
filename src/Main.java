df74ed50a57d61553f937d81d21250cc33ce7cb4
Maarten Westelinck <maartenwestelinck@hotmail.com>
2016-10-21 16:08:44 +0200

    finished exercises

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.util.Scanner;
 
 /**
  * Created by Admin on 20/10/2016.
@@ -9,24 +8,25 @@ import java.sql.Statement;
 public class Main {
 
     private static final String URL = "jdbc:mysql://localhost/bieren?autoReconnect=true&useSSL=false";
-    private static final String USER = "maarten";
-    private static final String PASSWORD = "maarten";
+    private static final String USER = "root";
+    private static final String PASSWORD = "vdab";
 
     public static void main(String[] args){
         try (
                 Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
                 Statement s = connection.createStatement()
             ){
-            System.out.println("Connectie geopend");
-            String bierenVerwijderen = "delete from bieren where alcohol is null";
-            System.out.println(s.execute(bierenVerwijderen));
+            connection.setAutoCommit(false);
+            String updateSql1 = "update bieren set brouwerid = 2 where alcohol >= 8.5";
+            String updateSql2 = "update bieren set brouwerid = 3 where brouwerid = 1";
+            String deleteBrouwer1 = "delete from brouwers where id = 1";
+            s.execute(updateSql1);
+            s.execute(updateSql2);
+            s.execute(deleteBrouwer1);
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-
-    private static void bierenVerwijderen(){
-
     }
 
 }
